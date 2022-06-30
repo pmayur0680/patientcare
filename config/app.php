cd26f8d7e5dec9b1921b6e60bbe960dcc32ae9d8
Mayur Patel <pmayur0680@gmail.com>
2022-06-30 10:25:52 -0500

    updated app.php

--- config/app.php
+++ config/app.php
@@ -6,7 +6,7 @@ use Cake\Database\Driver\Mysql;
 use Cake\Log\Engine\FileLog;
 use Cake\Mailer\Transport\MailTransport;
 use Cake\Core\Configure;
-Configure::write('debug', 0);
+
 
 return [
     /*
@@ -18,7 +18,8 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => filter_var(env('DEBUG', false), FILTER_VALIDATE_BOOLEAN),
+    'debug' => filter_var(env('APP_DEBUG', false), FILTER_VALIDATE_BOOLEAN),
+//    'debug' => filter_var(env('DEBUG', false), FILTER_VALIDATE_BOOLEAN),
 
     /*
      * Configure basic information about the application.
